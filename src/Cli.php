9b25672bb83bc2b87144528e9e292cc907314626
vqck <toridnc@gmail.com>
2021-12-04 21:17:57 +0300

    add Cli.php and change bin/brain-games

--- src/Cli.php
+++ src/Cli.php
@@ -2,10 +2,11 @@
 
 namespace Brain\Games\Cli;
 
+use function cli\line;
+use function cli\prompt;
+
 function welcome()
 {
-  use function cli\line;
-  use function cli\prompt;
 
   line('Welcome to the Brain Game!');
   $name = prompt('May I have your name?');
