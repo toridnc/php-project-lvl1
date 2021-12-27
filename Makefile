bd1c7d6b1437d12c9cc4ac2b590f14e424c9d2c6
vqck <toridnc@gmail.com>
2021-12-27 16:16:40 +0300

    corrected linter errors, added comments

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 install:
-		composer install
+	composer install
 
 brain-games:
-		./bin/brain-games
+	./bin/brain-games
 
 validate:
-		composer validate
+	composer validate
 
 lint:
-		composer exec --verbose phpcs -- --standard=PSR12 src bin
+	composer exec --verbose phpcs -- --standard=PSR12 src bin
--- src/Cli.php
+++ src/Cli.php
@@ -1,10 +1,25 @@
 <?php
-
+/**
+ * Cli File Doc Comment
+ *
+ * PHP version 7.4
+ *
+ * @category BrainGames
+ * @package  Welcome
+ * @author   toridnc <toridnc@gmail.com>
+ * @license  http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @link     https://github.com/toridnc/php-project-lvl1/tree/main
+ */
 namespace Brain\Games\Cli;
 
 use function cli\line;
 use function cli\prompt;
 
+/**
+ * Welcome function greets and asks for a name
+ * 
+ * @return string 'Hello, {$name}!'
+ */
 function welcome()
 {
     line('Welcome to the Brain Game!');
