4ce1140c45e72f75eaef07e1eaccaee5dbae816b
vqck <toridnc@gmail.com>
2022-01-11 22:13:02 +0300

    fix

---
+++ src/Games/GCD.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * GCD File Doc Comment
+ *
+ * PHP version 7.4
+ *
+ * @category BrainGames
+ * @package  GCD
+ * @author   toridnc <toridnc@gmail.com>
+ * @license  http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @link     https://github.com/toridnc/php-project-lvl1/tree/main
+ */
+namespace Src\Games\GCD;
+
+use function Brain\Games\Engine\engine;
+
+/**
+ * Bla-bla
+ * 
+ * Only 3 rounds.
+ * 
+ * @return string
+ */
+function gcd()
+{
+    
+}
